666ef30949fe419ad1584108d820bc70a0687dca
KeilaBrunetGonzalez <kbrunetgonzalez@outlook.com>
2024-08-02 19:04:55 -0400

    first commit

---
+++ cujae_custom/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Cujae Custom',
+    'version': '1.0',
+    'depends': ['base','web'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+
+}
